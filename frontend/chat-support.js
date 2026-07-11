b658d64a1cfbba76cb8d24645ee03e5741bad4b6
CodeTlhogii <letlhogonolonokias@gmail.com>
2026-07-10 19:54:10 -0700

    Updated all frontend pages with new design and auth menu

--- frontend/chat-support.js
+++ frontend/chat-support.js
@@ -1,3 +1,12 @@
+
+
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"></link>
+
+
+
+
 // ==================== CHAT SUPPORT SYSTEM ====================
 
 class ChatSupport {
